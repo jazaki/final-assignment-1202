b42d30ad02160a24293f1c700d5c8bed7afb6046
Junaid Arham <junaid_zaki@hotmail.com>
2022-08-01 10:05:30 -0400

    common-function: modify common function to include the count parameter

--- common.py
+++ common.py
@@ -1,10 +1,14 @@
 # Function to remove all null values and plot bar graph
-# TODO: need better comments
-def bar_graph_plotter(x_coord, y_coord, y_label, graph_title):
+def df_modifier(x_coord, y_coord):
   df_non_NaN = df[df[x_coord].notnull()][[x_coord, y_coord]]
-  new_df = (df_non_NaN.groupby(x_coord)[y_coord].value_counts(normalize=True).mul(100).round(2).sort_index()
+  return df_non_NaN.groupby(x_coord).filter(lambda x: len(x) > 26)
+  pass
+
+def bar_graph_plotter(x_coord, y_coord, y_label, graph_title):
+  filtered_df = df_modifier(x_coord, y_coord)
+  plot_df = (filtered_df.groupby(x_coord)[y_coord].value_counts(normalize=True).mul(100).round(2).sort_index()
     .to_frame(name=y_label)).reset_index()
 
-  fig = px.bar(new_df, x=x_coord, y=y_label, title=graph_title, color=y_coord)
+  fig = px.bar(plot_df, x=x_coord, y=y_label, title=graph_title, color=y_coord)
   fig.show()
   pass
